7b7edede7b7d4a83d894612fcb3305cf4e58bd65
Benedikt Eggers <be@spiegelburg.net>
2020-06-24 09:54:36 +0200

    Add project files.

---
+++ Simplic.GlobalKeyboardHook/MoveFocusBackHook.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Simplic.GlobalKeyboardHook
+{
+    public static class MoveFocusBackHook
+    {
+        private static Key _key;
+
+        public static void Setup(Key key = Key.Oem5)
+        {
+            EventManager.RegisterClassHandler(typeof(Window),
+            Keyboard.KeyDownEvent, new KeyEventHandler(keyUp), true);
+            _key = key;
+        }
+
+        private static void keyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == _key)
+            {
+                var tRequest = new TraversalRequest(FocusNavigationDirection.Previous);
+                var keyboardFocus = Keyboard.FocusedElement as UIElement;
+
+                if (keyboardFocus != null)
+                {
+                    keyboardFocus.MoveFocus(tRequest);
+                }
+
+                e.Handled = true;
+            }
+        }
+    }
+}
